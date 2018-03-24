e1d189b483193da351ae3cf6e97da570f7d4f323
Fahad Hossain <evilinfinite7@gmail.com>
2018-03-24 23:57:34 +0600

    standard-ify code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-snapshot'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+render(<App />, document.getElementById('root'))
+
+registerServiceWorker()
