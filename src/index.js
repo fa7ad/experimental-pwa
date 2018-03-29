fb8c97cdbd1a9c07dd56c7a2d31bd4c9eb53e7e4
Fahad Hossain <evilinfinite7@gmail.com>
2018-03-29 18:27:03 +0600

    add ssl certificates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { render } from 'react-snapshot'
+import { hydrate, render } from 'react-dom'
 
 import './index.css'
 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-render(<App />, document.getElementById('root'))
+const domRoot = document.getElementById('root')
+const renderFn = domRoot.hasChildNodes() ? hydrate : render
+
+renderFn(<App />, domRoot)
 
 registerServiceWorker()
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -9,29 +9,34 @@ const isLocalhost = Boolean(
 )
 
 export default function register () {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location)
-    if (publicUrl.origin !== window.location.origin) {
-      return
-    }
-
-    window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
-
-      if (isLocalhost) {
-        checkValidServiceWorker(swUrl)
-
-        navigator.serviceWorker.ready.then(() => {
-          console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://goo.gl/SC7cgQ'
-          )
-        })
-      } else {
-        registerValidSW(swUrl)
-      }
-    })
+  if (
+    process.env.NODE_ENV !== 'production' &&
+    !('serviceWorker' in navigator)
+  ) {
+    return
   }
+
+  const publicUrl = new URL(process.env.PUBLIC_URL, window.location)
+  if (publicUrl.origin !== window.location.origin) {
+    return
+  }
+
+  window.addEventListener('load', () => {
+    const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+
+    if (isLocalhost) {
+      checkValidServiceWorker(swUrl)
+
+      navigator.serviceWorker.ready.then(() => {
+        console.log(
+          'This web app is being served cache-first by a service ' +
+            'worker. To learn more, visit https://goo.gl/SC7cgQ'
+        )
+      })
+    } else {
+      registerValidSW(swUrl)
+    }
+  })
 }
 
 function registerValidSW (swUrl) {
